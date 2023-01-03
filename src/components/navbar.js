07bbf60446a5782ae1991297e8273fcee598eb8f
Nicolas Araújo <nicolasaraujo@gmail.com>
2023-01-03 19:47:40 -0300

    feat: page not found

---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+export default function Navbar(){
+    return(
+        <NavbarContainer>
+            
+        </NavbarContainer>
+    )
+}
+
+const NavbarContainer= styled.header`
+
+`
