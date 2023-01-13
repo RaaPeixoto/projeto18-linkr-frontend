63370a3639e896a3e2b2e6bfe97b32c5d6541c3d
Nicolas Araújo <nicolasaraujo@gmail.com>
2023-01-13 17:14:07 -0300

    refactor: chamada de funcãoes.

--- src/pages/timeline/timeline.js
+++ src/pages/timeline/timeline.js
@@ -16,9 +16,16 @@ export default function Timeline() {
   const config = { headers: { Authorization: `Bearer ${token}` } };
 
   const [listPosts, setListPosts] = useState(null);
+  const [isFollowingSomeone, setIsFollowingSomeone] = useState(false);
   const [reloadPosts, setReloadPosts] = useState({});
+  console.log(listPosts);
 
   useEffect(() => {
+    buscarPosts();
+    fetchFollowing();
+  }, [reloadPosts]);
+
+  function buscarPosts() {
     axios
       .get(`${BASE_URL}/posts`, config)
       .then((res) => {
@@ -37,31 +44,47 @@ export default function Timeline() {
           "An error occured while trying to fetch the posts, please refresh the page"
         );
       });
-  }, [reloadPosts]);
+  }
+
+  function fetchFollowing() {
+    console.log('oi')
+    // axios.get("/followers", config).then().catch();
+  }
+
+  function renderPosts(postData) {
+    return postData.repost === true ? (
+      <Repost
+        key={`${postData.id}-${postData.repostId}`}
+        postData={postData}
+        setReloadPosts={setReloadPosts}
+      />
+    ) : (
+      <Post
+        key={postData.id}
+        postData={postData}
+        image={postData.image}
+        username={postData.username}
+        setReloadPosts={setReloadPosts}
+      />
+    );
+  }
+
+  if (!listPosts) {
+    return (
+      <ScreenBackgroundColor
+        titlePage="timeline"
+        setReloadPosts={setReloadPosts}
+      >
+        Loading...
+      </ScreenBackgroundColor>
+    );
+  }
 
   return (
     <ScreenBackgroundColor titlePage="timeline" setReloadPosts={setReloadPosts}>
-      {!listPosts
-        ? "Loading..."
-        : listPosts.length === 0
-        ? "There are no posts yet"
-        : listPosts.map((postData) =>
-            postData.repost === true ? (
-              <Repost
-                key={`${postData.id}-${postData.repostId}`}
-                postData={postData}
-                setReloadPosts={setReloadPosts}
-              />
-            ) : (
-              <Post
-                key={postData.id}
-                postData={postData}
-                image={postData.image}
-                username={postData.username}
-                setReloadPosts={setReloadPosts}
-              />
-            )
-          )}
+      {listPosts.length !== 0
+        ? listPosts.map(renderPosts)
+        : "No posts found from your friends"}
     </ScreenBackgroundColor>
   );
 }
